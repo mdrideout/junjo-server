994e53c286954f1aa19d26895415eb4f2a137120
Matthew Rideout <boon4376@gmail.com>
2025-02-18 20:30:48 -0500

    Example graph layout setup.

--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -1,75 +1,110 @@
-import { addEdge, ReactFlow, useEdgesState, useNodesState } from '@xyflow/react'
-
 import '@xyflow/react/dist/style.css'
-import { useCallback } from 'react'
 
-const initialNodes = [
-  {
-    id: '4347435328',
-    data: { label: 'CountNode' },
-    position: { x: 0.0, y: 0.0 },
-  },
-  {
-    id: '4347435664',
-    data: { label: 'IncrementNode' },
-    position: { x: 0.0, y: 100 },
-  },
-  {
-    id: '4347436000',
-    data: { label: 'SetWarningNode' },
-    position: { x: 150, y: 200 },
-  },
-  {
-    id: '4347436336',
-    data: { label: 'FinalNode' },
-    position: { x: 0.0, y: 300 },
-  },
-]
-const initialEdges = [
-  {
-    id: '4347435328-4347435664',
-    source: '4347435328',
-    target: '4347435664',
-    label: null,
-  },
-  {
-    id: '4347435664-4347436000',
-    source: '4347435664',
-    target: '4347436000',
-    label: 'count_over_10',
-  },
-  {
-    id: '4347436000-4347436336',
-    source: '4347436000',
-    target: '4347436336',
-    label: null,
-  },
-  {
-    id: '4347435664-4347436336',
-    source: '4347435664',
-    target: '4347436336',
-    label: null,
-  },
-]
+import {
+  addEdge,
+  Background,
+  ConnectionLineType,
+  Panel,
+  ReactFlow,
+  useEdgesState,
+  useNodesState,
+  type Edge,
+  type Node,
+} from '@xyflow/react'
+import dagre, { type GraphLabel, type LayoutConfig } from '@dagrejs/dagre'
+
+import { useCallback } from 'react'
+import { initialNodes, initialEdges } from './example-rf-data'
+
+// Layout library: https://github.com/dagrejs/dagre/wiki
+const dagreGraph = new dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}))
+
+const nodeWidth = 150
+const nodeHeight = 40
+
+const getLayoutedElements = (nodes, edges, direction = 'TB') => {
+  const isHorizontal = direction === 'LR'
+  dagreGraph.setGraph({ rankdir: direction, nodesep: 300, ranksep: 40 })
+
+  nodes.forEach((node) => {
+    node.style = {
+      width: nodeWidth,
+      height: nodeHeight,
+      wordBreak: 'break-all',
+      padding: '4px 6px',
+      lineHeight: '1.2',
+      fontSize: '11px',
+      display: 'grid',
+      placeItems: 'center',
+      justifyItems: 'center',
+    }
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight })
+  })
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target)
+  })
+
+  dagre.layout(dagreGraph)
+
+  const newNodes = nodes.map((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id)
+    const newNode = {
+      ...node,
+      targetPosition: isHorizontal ? 'left' : 'top',
+      sourcePosition: isHorizontal ? 'right' : 'bottom',
+      // We are shifting the dagre node position (anchor=center center) to the top left
+      // so it matches the React Flow node anchor point (top left).
+      position: {
+        x: nodeWithPosition.x - nodeWidth / 2,
+        y: nodeWithPosition.y - nodeHeight / 2,
+      },
+    }
+
+    return newNode
+  })
+
+  return { nodes: newNodes, edges }
+}
+
+const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(initialNodes, initialEdges, 'LR')
 
 export default function DashboardIndex() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes)
+  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges)
 
-  const onConnect = useCallback((params: any) => setEdges((eds) => addEdge(params, eds)), [setEdges])
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge({ ...params, type: ConnectionLineType.SmoothStep, animated: true }, eds)),
+    []
+  )
+  const onLayout = useCallback(
+    (direction) => {
+      const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(nodes, edges, direction)
+
+      setNodes([...layoutedNodes])
+      setEdges([...layoutedEdges])
+    },
+    [nodes, edges]
+  )
 
   return (
-    <div>
-      <p>Sample ReactFlow from output.</p>
-      <div className="w-dvw h-dvh text-black">
-        <ReactFlow
-          nodes={initialNodes}
-          edges={initialEdges}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-          onConnect={onConnect}
-        />
-      </div>
+    <div className="w-dvw h-dvh">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        connectionLineType={ConnectionLineType.SmoothStep}
+        defaultViewport={{ zoom: 1.0, x: 20, y: 20 }}
+        style={{ backgroundColor: 'transparent', color: '#000' }}
+      >
+        {/* <Panel position="top-left">
+          <button onClick={() => onLayout('TB')}>vertical layout</button>
+          <button onClick={() => onLayout('LR')}>horizontal layout</button>
+        </Panel> */}
+        <Background />
+      </ReactFlow>
     </div>
   )
 }
