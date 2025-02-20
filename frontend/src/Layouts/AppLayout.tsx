e4f3a59c092fbd8cec3f39632764270918f06ce2
Matthew Rideout <boon4376@gmail.com>
2025-02-19 19:23:41 -0500

    FE reactflow example imported.

--- frontend/src/Layouts/AppLayout.tsx
+++ frontend/src/Layouts/AppLayout.tsx
@@ -1,8 +1,8 @@
 import { Outlet } from 'react-router'
-import { Navbar } from '../catalyst/navbar'
-import { Sidebar } from '../catalyst/sidebar'
-import { SidebarLayout } from '../catalyst/sidebar-layout'
-import SidebarMenu from '../catalyst/sidebar-menu'
+import { Navbar } from '../components/catalyst/navbar'
+import { Sidebar } from '../components/catalyst/sidebar'
+import { SidebarLayout } from '../components/catalyst/sidebar-layout'
+import SidebarMenu from '../components/catalyst/sidebar-menu'
 
 export function AppLayout() {
   return (
--- frontend/src/dashboard/Dashboard.tsx
+++ frontend/src/dashboard/Dashboard.tsx
@@ -1,8 +1,11 @@
+import ReactFlowCanvas from '../react-flow/react-flow-canvas'
+
 export default function Dashboard() {
   return (
     <>
       <div>
         <h1>Dashboard</h1>
+        <ReactFlowCanvas />
       </div>
     </>
   )
