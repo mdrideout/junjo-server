f6649fd8a969012bdb7841622d6af4cd68f72bdb
Matthew Rideout <boon4376@gmail.com>
2025-02-21 23:26:07 -0500

    Removed RR7 files. Parity reached with new decoupled architecture.

--- .rr7-archive/app/routes/dashboard/index.tsx
+++
@@ -1,9 +0,0 @@
-import ReactFlowCanvas from './react-flow-canvas'
-
-export default function DashboardIndex() {
-  return (
-    <div className="w-dvw h-dvh">
-      <ReactFlowCanvas />
-    </div>
-  )
-}
