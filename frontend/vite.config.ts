5ce06b5cdae81277de0a9a2e280ee7e8b85024a5
Matthew Rideout <boon4376@gmail.com>
2025-02-22 00:04:17 -0500

    Docker compose with hot reloading on FE and BE working.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    port: 5173,
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      clientPort: 4951, // must be mapped in docker compose to 5177
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
 })
