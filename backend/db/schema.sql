531641b618edc10b7fdd1e7ebed4004fed954fc9
Matthew Rideout <boon4376@gmail.com>
2025-02-26 18:33:23 -0500

    Should be setup to store state as a JSONB column inside sqlite - not tested. Need to do library side now.

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE workflow_logs (
   id text PRIMARY KEY NOT NULL UNIQUE,
   exec_id text NOT NULL,
-  name text NOT NULL,
   type TEXT NOT NULL CHECK (type IN ('start', 'end')),
   event_time_nano INTEGER NOT NULL,
   -- allows for nanosecond precision
-  ingestion_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  ingestion_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  state JSONB NOT NULL
 );
