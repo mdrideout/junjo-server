b0a997cbd03f8af1af521c0d8b618b290447cd93
Matthew Rideout <boon4376@gmail.com>
2025-02-27 23:26:16 -0500

    Formatting improvements

--- frontend/src/workflow-logs/WorkflowLogPage.tsx
+++ frontend/src/workflow-logs/WorkflowLogPage.tsx
@@ -29,17 +29,33 @@ export default function WorkflowLogPage() {
     return <ErrorPage title={'404: Not Found'} message={'No workflows found with this execution id.'} />
   }
 
+  // Human readable start ingest time
+  const date = new Date(workflowLogs[0]?.IngestionTime)
+  const readableStart = date.toLocaleString()
+
+  // Parse duration
+  const durationNano = workflowLogs[1]?.EventTimeNano - workflowLogs[0]?.EventTimeNano
+  const durationMs = durationNano / 1e6
+  const durationMsRounded = Math.round(durationMs * 100) / 100
+
   return (
     <div className={'p-5'}>
-      <div className={'mb-3 flex gap-x-3'}>
-        <Link to={'/logs'} className={'hover:underline'}>
-          logs
-        </Link>{' '}
-        <div>&gt;</div> <div>{ExecID}</div>
+      <div className={'px-2'}>
+        <div className={'mb-1 flex gap-x-3'}>
+          <Link to={'/logs'} className={'hover:underline'}>
+            logs
+          </Link>{' '}
+          <div>&rarr;</div>
+          <div>{ExecID}</div>
+        </div>
+        <div className={'text-zinc-400 text-xs'}>
+          {readableStart} &mdash; {durationMsRounded} ms
+        </div>
       </div>
-      <hr className={'my-5'} />
-      <div className={''}>
-        <div className="flex gap-5">
+      <hr className={'my-6'} />
+      <div className={'px-2'}>
+        <div className="flex gap-10">
+          {workflowLogs.length === 0 && <p>No logs found for this workflow.</p>}
           {workflowLogs.map((log) => {
             // Decode the base64 encoded state into stringified json,
             // then format the JSON string with 2 spaces for indentation
@@ -52,18 +68,18 @@ export default function WorkflowLogPage() {
                 </p>
                 <p>
                   <strong>Execution ID:</strong> {log.ExecID}
-                </p> */}
+                </p>
                 <p>
                   <strong>Nano time number:</strong> {log.EventTimeNano}
                 </p>
                 <p>
                   <strong>Ingest time:</strong> {log.IngestionTime}
-                </p>
+                </p> */}
                 <p>
-                  <strong>Type:</strong> {log.Type}
+                  <strong>{log.Type}</strong>
                 </p>
                 <div>
-                  <strong>State:</strong> <pre>{displayState}</pre>
+                  <pre>{displayState}</pre>
                 </div>
               </div>
             )
