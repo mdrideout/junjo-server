1fea841734466802f04d8d132e92f8944fdd15dd
Matthew Rideout <boon4376@gmail.com>
2025-02-28 00:26:54 -0500

    lint fix

--- frontend/src/workflow-logs/WorkflowList.tsx
+++ frontend/src/workflow-logs/WorkflowList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { WorkflowMetadatum } from './schemas'
 import { fetchWorkflowMetadataList } from './fetch/fetch-workflow-metadata'
 import { useNavigate } from 'react-router'
