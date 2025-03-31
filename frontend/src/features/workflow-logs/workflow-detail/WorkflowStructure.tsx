fc2746fbce5c4fdd95f8f69a8c2e219a35f75766
Matthew Rideout <boon4376@gmail.com>
2025-03-30 23:20:40 -0400

    Temp rendering improvements for ReactFlow

--- frontend/src/features/workflow-logs/workflow-detail/WorkflowStructure.tsx
+++ frontend/src/features/workflow-logs/workflow-detail/WorkflowStructure.tsx
@@ -15,7 +15,7 @@ export default function WorkflowStructure(props: WorkflowStructureProps) {
   const { graph } = props
 
   return (
-    <div className={`w-full h-[60px] mb-5`}>
+    <div className={`w-full h-[250px] mb-5`}>
       <ReactFlowJunjoGraph junjoGraph={graph} direction={ReactFlowGraphDirection.LR} />
     </div>
   )
